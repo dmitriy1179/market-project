1a2b7a24ff3e2e976330f75ffd3eedbd8776582e
Dmitriy1179 <chda7911@gmail.com>
2020-12-22 14:37:16 +0200

    Updates

---
+++ precache-manifest.202ffc7422a1985a1894e9951f683700.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a391a8dc380afab27600186fc0e7bd7",
+    "url": "/market-project/index.html"
+  },
+  {
+    "revision": "d6accb63549e3cc35be5",
+    "url": "/market-project/static/css/2.51692fcd.chunk.css"
+  },
+  {
+    "revision": "79f09b801212fd29808c",
+    "url": "/market-project/static/css/main.0cb2e7f6.chunk.css"
+  },
+  {
+    "revision": "d6accb63549e3cc35be5",
+    "url": "/market-project/static/js/2.83291b83.chunk.js"
+  },
+  {
+    "revision": "79f09b801212fd29808c",
+    "url": "/market-project/static/js/main.0aba115d.chunk.js"
+  },
+  {
+    "revision": "8bcf25aeb85323b6430a",
+    "url": "/market-project/static/js/runtime~main.79ec4e3a.js"
+  },
+  {
+    "revision": "9dc251b48ef10361c27a4c6e956c1e88",
+    "url": "/market-project/static/media/exchange1.9dc251b4.png"
+  }
+]);
