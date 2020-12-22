72b962c0fa5e1a1bc8111873db58be111fbf1a19
Dmitriy1179 <chda7911@gmail.com>
2020-12-22 16:24:02 +0200

    Updates

---
+++ precache-manifest.190cb3c77bcea8f12dca5154b9b38b0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beaa4ee53e865deb4508f68c57ccc48d",
+    "url": "/market-project/login/index.html"
+  },
+  {
+    "revision": "d6accb63549e3cc35be5",
+    "url": "/market-project/login/static/css/2.51692fcd.chunk.css"
+  },
+  {
+    "revision": "79f09b801212fd29808c",
+    "url": "/market-project/login/static/css/main.0cb2e7f6.chunk.css"
+  },
+  {
+    "revision": "d6accb63549e3cc35be5",
+    "url": "/market-project/login/static/js/2.83291b83.chunk.js"
+  },
+  {
+    "revision": "79f09b801212fd29808c",
+    "url": "/market-project/login/static/js/main.0aba115d.chunk.js"
+  },
+  {
+    "revision": "354d99eb3b2057728f40",
+    "url": "/market-project/login/static/js/runtime~main.1ec185b4.js"
+  },
+  {
+    "revision": "9dc251b48ef10361c27a4c6e956c1e88",
+    "url": "/market-project/login/static/media/exchange1.9dc251b4.png"
+  }
+]);
